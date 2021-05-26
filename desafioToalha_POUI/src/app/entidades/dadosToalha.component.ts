bdb859572c3adc5a72d81e4acf44ad36f8da880f
Eduardo Paro de Simoni <eduardo.system96@gmail.com>
2021-05-25 23:47:47 -0300

    Add files via upload

---
+++ desafioToalha_POUI/src/app/entidades/dadosToalha.component.ts
@@ -0,0 +1,13 @@
+export class dadosToalha{
+    codigo:string = '';
+    nome:string = '';
+    nomereduzido:string='';
+    loja:string   = '';
+    endereco:string='';
+    tipo:string='';
+    estado:string='';
+    municipio:string='';
+
+
+
+}
